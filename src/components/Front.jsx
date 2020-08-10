44d756bdc297bf0bc8ad5eb8c90884f6bb6b1bb5
Marshal Welgama <marshalwelgama@gmail.com>
2020-08-10 21:48:10 +1000

    NEW: added bootstrap and fixed front

--- src/components/Front.jsx
+++ src/components/Front.jsx
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
 import "./Front.css";
+import 'bootstrap/dist/css/bootstrap.css';
+import { Container, Row, Col } from 'react-bootstrap';
 class Logo extends Component {
     state = {  }
     render() { 
         return ( 
+            
             <div className="login-page">
             <img className="logo-img" src="/logo.png" ></img>
-            <a className="login-button">LOGIN WITH SPOTIFY</a>
+           
+            <div className="description-text">
+               
+            <input className="form-box py-4 mb-5" type="email" id="email" placeholder="       Enter Your Email" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"/>
             </div>
-         );
+        
+            {/* <a className="login-button">LOGIN WITH SPOTIFY</a> */}
+            </div>
+        )
     }
 }
  
