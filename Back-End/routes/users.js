71a16553da0cd1fcc4f3da8bc7dbe9a56178ad5e
aseem-thakar <41178612+aseem-thakar@users.noreply.github.com>
2020-09-11 21:50:01 +1000

    arch: using JSON body for all requests

--- Back-End/routes/users.js
+++ Back-End/routes/users.js
@@ -58,7 +58,7 @@ router.get("/current", function (req, res) {
 });
 
 // Getting one user
-router.get("/:id", getUser, (req, res) => {
+router.get("/", getUser, (req, res) => {
   res.json(res.user);
 });
 
@@ -82,7 +82,7 @@ router.get("/:id", getUser, (req, res) => {
 // });
 
 // Updating a user
-router.patch("/:id", getUser, async (req, res) => {
+router.patch("/", getUser, async (req, res) => {
   if (req.body.name != null) {
     res.user.name = req.body.name;
   }
@@ -108,7 +108,7 @@ router.patch("/:id", getUser, async (req, res) => {
 });
 
 // Deleting a user
-router.delete("/:id", async (req, res) => {
+router.delete("/", async (req, res) => {
   try {
     await User.deleteOne({ userId: req.body.id });
     res.json({ message: "Deleted user" });
@@ -120,7 +120,7 @@ router.delete("/:id", async (req, res) => {
 async function getUser(req, res, next) {
   let user;
   try {
-    user = await User.find({ userId: req.params.id });
+    user = await User.find({ userId: req.body.id });
     if (user == null) {
       return res.status(404).json({ message: "Cannot find user" });
     }
